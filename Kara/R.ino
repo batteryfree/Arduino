16d038989144e0f24a72267d25b62068a18765de
batteryfree <44664353+batteryfree@users.noreply.github.com>
2019-09-17 19:42:22 +0300

    added

---
+++ Kara/R.ino
@@ -0,0 +1,25 @@
+﻿//work
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(9, 10);
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+char alarm[]="911";
+String str; 
+int switchPin = 2;
+
+void setup() {
+  radio.begin();
+  radio.openWritingPipe(pipe);
+  pinMode(switchPin, INPUT);
+}
+
+void loop() {
+  if (digitalRead(switchPin) == HIGH){
+  str="911";
+  str.toCharArray(alarm,sizeof(alarm)+2);
+  radio.powerUp();
+    for (int i=1;i<10;i++){
+      radio.write(alarm, sizeof(alarm)+2);  
+      delay(100);
+}}}
---
+++ Kara/T.ino
@@ -0,0 +1,47 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <SoftwareSerial.h>
+#include <DFPlayer_Mini_Mp3.h>
+
+RF24 radio(9, 10);
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+String str;
+int r=7;
+int t=0;
+
+void setup() {
+  Serial.begin(9600);
+  mp3_set_serial(Serial);
+  delay (100);
+  mp3_set_volume(25);
+  delay (100);
+  mp3_play(1);
+  delay (100);
+  delay(10000);
+  radio.begin();
+  radio.openReadingPipe(1, pipe);
+  radio.startListening();
+  pinMode(r,OUTPUT);
+  digitalWrite(r, HIGH);
+}
+
+void loop() {
+//radio.openReadingPipe(1, pipe);
+if (radio.available()){
+//  Serial.println("in port");
+  char d[32]="" ;
+  radio.read(&d, sizeof(d));
+  str = d;
+// Serial.println(str);
+  if (str == "911"){
+    digitalWrite(r, LOW);
+    t=1;
+   }}
+if (t>0){
+ t=0;
+ delay (100);
+  mp3_play(2);
+   delay (10000);
+}}
+
